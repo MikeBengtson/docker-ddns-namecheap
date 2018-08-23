11b5b080f6bd1ad7fe1690f4bbda73b6015c85db
Alejandro Lorente <alejandro.lorente@qvantel.com>
2018-08-23 15:31:43 +0200

    clean the concatenating commands

--- configure.sh
+++ configure.sh
@@ -2,9 +2,9 @@
 
 # Script to configure the update-ip script with the params given at the container run.
 
-sed -i.bak "s/^DOMAIN=.*$/DOMAIN=\"$DOMAIN\"/g" /usr/bin/NamecheapDdnsUpdate.sh && \
-sed -i.bak "s/^PASSWORD=.*$/PASSWORD=\"$PASSWORD\"/g" /usr/bin/NamecheapDdnsUpdate.sh && \
-sed -i.bak "s/^HOSTS=.*$/HOSTS=\($HOSTS\)/g" /usr/bin/NamecheapDdnsUpdate.sh && \
+sed -i.bak "s/^DOMAIN=.*$/DOMAIN=\"$DOMAIN\"/g" /usr/bin/NamecheapDdnsUpdate.sh
+sed -i.bak "s/^PASSWORD=.*$/PASSWORD=\"$PASSWORD\"/g" /usr/bin/NamecheapDdnsUpdate.sh
+sed -i.bak "s/^HOSTS=.*$/HOSTS=\($HOSTS\)/g" /usr/bin/NamecheapDdnsUpdate.sh
 sed -i.bak "s/^EMAIL=.*$/EMAIL=\"$EMAIL\"/g" /usr/bin/NamecheapDdnsUpdate.sh
 
 exit 0
